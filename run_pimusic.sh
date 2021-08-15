25d68be63cafec1d0e93b90928b07155e7e4c7da
Kamil Porwit <kamilporwit93@gmail.com>
2021-08-15 11:05:30 +0200

    add HTTPS support

--- run_pimusic.sh
+++ run_pimusic.sh
@@ -13,10 +13,10 @@
 #                -h displays help.
 #                -S Enables SSL encryption (all flags below are valid only when this one is enabled).
 #                -s <SSL port> Port for SSL connection.
-#                -c <path to SSL certificate> Path with the certificate of Your domain.
-#                -k <path to private key of the SSL certificate> Path with the private key of the certificate.
+#                -c <path to SSL certificate> Path to the certificate of Your domain.
+#                -k <path to private key of the SSL certificate> Path to the private key of the certificate.
 #        AUTHOR:  Kamil Porwit (), kamilporwit93@gmail.com
-#       VERSION:  1.0
+#       VERSION:  2.0
 #       CREATED:  08/09/2021 09:02:58 AM CEST
 #===============================================================================
 
@@ -30,6 +30,20 @@ path_to_private_key=''
 
 print_usage() {
   printf "Usage:\n ./run_pimusic.sh -P <The path which contains Your music> -p <Port where the cherrymusic will be published (e.g 8181)> -V <Version of pimusic to run (e.g. v0.0.2)>\n\t -S <enables SSL encryption> -s <Port for the SSL encryption> -c <Path to the SSL certyficate> -k <Path to the private key of the certificate>\n"
+}
+
+readable_by_others() {
+  local passed_file="$1"
+  permissions=$(stat --print=%A $passed_file)
+  ro_byte=$(echo $permissions | cut -c 8)
+  if [ -L "$passed_file" ]; then
+    printf "WARNING: the file %s is a symbolic link. Please make sure that the orignal file has read permission for others.\n" "$passed_file"
+  fi
+  if [ "$ro_byte" = "r" ]; then
+    return 0
+  else
+    return 1
+  fi
 }
 
 while getopts 'P:p:V:Ss:c:k:h' flag; do
@@ -66,8 +80,18 @@ fi
 
 if [ "$ssl_enabled" = "True" ]; then
 
+  if ! readable_by_others "${path_to_certificate}"; then
+    printf "ERROR: certificate file is not readable by others.\n"
+    exit 31
+  fi
+
   if [[ ! (-e "${path_to_certificate}" || -d "${path_to_certificate}" || -r "${path_to_certificate}") ]]; then
     printf "ERROR: certificate path: %s either does not exists, is not a directory or is not readable by %s user.\n" "${path_to_certificate}" "${USER}"
+    exit 31
+  fi
+
+  if ! readable_by_others "${path_to_private_key}"; then
+    printf "ERROR: private key file is not readable by others.\n"
     exit 31
   fi
   
@@ -92,12 +116,13 @@ if [ "$ssl_enabled" = "False" ]; then
     --restart=unless-stopped \
     --mount source=pimusic_share_vol,target=/home/pimusic/.local \
     --mount source=pimusic_config_vol,target=/home/pimusic/.config \
-    "pimusic:${version}"
+    "kporwit/pimusic:${version}"
 else
   printf "Starting up pimusic container with SSL support (version: %s) on port %s (SSL port %s) with music path %s\n" "${version}" "${publish_port}" "${ssl_port}" "${path_to_music}"
   docker run -d --name pimusic \
     -p "${publish_port}:${publish_port}" \
     -e PUBLISH_PORT="${publish_port}" \
+    -p "${ssl_port}:${ssl_port}" \
     -e SSL_ENABLED="True" \
     -e SSL_PORT="${ssl_port}" \
     -v "${path_to_certificate}:/home/pimusic/certs/server.crt" \
@@ -106,5 +131,5 @@ else
     --restart=unless-stopped \
     --mount source=pimusic_share_vol,target=/home/pimusic/.local \
     --mount source=pimusic_config_vol,target=/home/pimusic/.config \
-    "pimusic:${version}"
+    "kporwit/pimusic:${version}"
 fi
